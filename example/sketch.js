5a0640ddcd74979f849e2f4f9971a4c9e01561c7
Kenichi Wakabayashi <kwaka1208@crssrds.jp>
2021-09-20 20:09:16 +0900

    add sample program

--- example/sketch.js
+++ example/sketch.js
@@ -1,7 +1,15 @@
 function setup() {
   // put setup code here
+  createCanvas(600,600);
 }
 
 function draw() {
   // put drawing code here
+  if(mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  r = 80;
+  ellipse(mouseX, mouseY, r, r)
 }
