6974b4bdd3ac7a0fc0248dbed37bfe3dfee423e7
yuna1412 <85538944+yuna1412@users.noreply.github.com>
2021-10-07 15:06:45 +0900

    aaaaaaaa

--- projects/circle/sketch.js
+++ projects/circle/sketch.js
@@ -10,12 +10,13 @@ function draw() {
     r = random(255);
     g = random(255);
     b = random(255);
+    fill(r, g, b);
+    r = 100;
+    ellipse(mouseX, mouseY, r, r);
+  }
   } else {
     r = 255;
     g = 255;
     b = 255;
   }
-  fill(r, g, b);
-  r = 100;
-  ellipse(mouseX, mouseY, r, r);
-}
+  
